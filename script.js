41f6e19713e3e157dd9d550c76b46b69aa60a79e
natsch2020 <natsch2020@gmail.com>
2023-08-03 18:50:44 -0500

    Made playRound() in script.js
    playRound() now has the functionality it is described to have.
    Made playerSelection all lowercase to compare to computerSelection.
    Checked for ties first since that is easy to do.
    Used switch-case to determine winner.
    Default, in case of bad input, is a loss.

--- script.js
+++ script.js
@@ -4,19 +4,55 @@ function getComputerChoice() {
     choice = Math.floor(Math.random() * 3); //Generates a random integer between 0 and 2
 
     if(choice === 0) {
-        return "Rock";
+        return "rock";
     }
     else if(choice === 1) {
-        return "Paper";
+        return "paper";
+    }
+    else if(choice === 2) {
+        return "scissors";
     }
     else {
-        return "Scissors"
+        return;
     }
 }
 
 //Determines the winner of one round of the game and returns a string that indicates if the player won, lost, or tied
 function playRound(playerSelection, computerSelection) {
+    p = playerSelection.toLowerCase();
 
+    if(p === computerSelection) {
+        return "You both chose " + p + ". It's a tie.";
+    }
+
+    switch(computerSelection) {
+        case "rock":
+            if(p === "paper") {
+                return "You win! Paper beats rock.";
+            }
+            else {
+                return "You lose! Rock beats scissors.";
+            }
+        
+        case "paper":
+            if(p === "scissors") {
+                return "You win! Scissors beats paper.";
+            }
+            else {
+                return "You lose! Paper beats rock.";
+            }
+
+        case "scissors":
+            if(p === "rock") {
+                return "You win! Rock beats scissors.";
+            }
+            else {
+                return "You lose! Scissors beats paper.";
+            }
+
+        default:
+            return "You lose! " + playerSelection + " loses to " + computerSelection + ".";
+    }
 }
 
 //Has the player play 5 rounds of the game and keeps score to report who won based on the best of 5
