add5d6d5c4ce9f4a65ebfbf8a70e5345feff2069
natsch2020 <natsch2020@gmail.com>
2023-08-03 13:47:27 -0500

    Created html and js files for Rock Paper Scissors
    Created index.html to run the script.
    Created script.js as the script for index.html.
    Created skeletons for 3 functions in script.js.
    Added comments to communicate what each function will do once finished.

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//Randomly selects a choice of rock, paper, or scissors for the computer opponent
+function getComputerChoice() {
+
+}
+
+//Determines the winner of one round of the game and returns a string that indicates if the player won, lost, or tied
+function playRound(playerSelection, computerSelection) {
+
+}
+
+//Has the player play 5 rounds of the game and keeps score to report who won based on the best of 5
+function game() {
+
+}
