19201bf967f495cbdb5393805d28b1956dc47d82
natsch2020 <natsch2020@gmail.com>
2023-08-03 17:37:47 -0500

    Made getcomputerChoice() in script.js
    getcomputerChoice() now has the functionality it is described to have.
    Did this by using Math.floor(Math.random() * 3);
    which generates a random integer between 0 and 2,
    each of which corresponds to "Rock", "Paper", or "Scissors".

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
 //Randomly selects a choice of rock, paper, or scissors for the computer opponent
 function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3); //Generates a random integer between 0 and 2
 
+    if(choice === 0) {
+        return "Rock";
+    }
+    else if(choice === 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors"
+    }
 }
 
 //Determines the winner of one round of the game and returns a string that indicates if the player won, lost, or tied
